3800d3ba5ad1c914c7bbc29bafa840ad190b9ef9
Garry Hill <garry@magnetised.net>
2023-04-18 16:53:58 +0100

    make right-hand bottom row more useful

--- keyboards/ergodox_ez/keymaps/garry/keymap.c
+++ keyboards/ergodox_ez/keymaps/garry/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,         KC_W,    KC_E,    KC_R,         KC_T, KC_BSPACE,                                                                   KC_BSPACE,              KC_Y, KC_U,     KC_I,        KC_O,        KC_P,             KC_BSLASH,
     KC_GRAVE,       KC_A,         KC_S,    KC_D,    KC_F,         KC_G,                                                                                                      KC_H, KC_J,     KC_K,        KC_L,        KC_SCOLON,        KC_QUOTE,
     KC_LSFT,        LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,         KC_B, MT(MOD_HYPR, KC_ENTER),                                                      MT(MOD_HYPR, KC_ENTER), KC_N, KC_M,     KC_COMMA,    KC_DOT,      RCTL_T(KC_SLASH), KC_RSHIFT,
-    LT(1,KC_GRAVE), KC_SPC,       KC_LEAD, KC_LALT, LT(3,KC_END),                                                                                                                  KC_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_TRNS,   MO(1),
+    LT(1,KC_GRAVE), KC_SPC,       KC_LEAD, KC_LALT, LT(3,KC_END),                                                                                                                  KC_EQUAL, KC_LALT,     KC_TRNS,     KC_RCTL,          MO(1),
                                                                                         LT(2, KC_ESC), TG(1),             KC_LOCK,   LCTL_T(KC_ESC),
                                                                                                        KC_HOME,           KC_PGUP,
                                                                         LGUI_T(KC_SPC), LT(4, KC_ESC), LT(3,KC_END),      KC_PGDOWN, LT(4, KC_ESC),  KC_SPC
