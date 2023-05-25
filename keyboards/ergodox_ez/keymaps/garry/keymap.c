a5609c4c199841d87fe71e205c1d014272bcbd8f
Garry Hill <garry@magnetised.net>
2023-05-25 12:01:23 +0100

    use an un-shifted hyper

--- keyboards/ergodox_ez/keymaps/garry/keymap.c
+++ keyboards/ergodox_ez/keymaps/garry/keymap.c
@@ -15,6 +15,8 @@
 #define NO_PIPE_ALT KC_GRAVE
 #define NO_BSLS_ALT KC_EQUAL
 
+#define MOD_G_HYPR (MOD_LCTL | MOD_LALT | MOD_LGUI)
+
 enum custom_keycodes {
 #ifdef ORYX_CONFIGURATOR
     VRSN = EZ_SAFE_RANGE,
@@ -33,10 +35,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,         KC_1,         KC_2,    KC_3,    KC_4,         KC_5, KC_DELETE,                                                                   KC_DELETE,              KC_6, KC_7,     KC_8,        KC_9,        KC_0,             KC_MINUS,
     KC_TAB,         KC_Q,         KC_W,    KC_E,    KC_R,         KC_T, KC_BSPACE,                                                                   KC_BSPACE,              KC_Y, KC_U,     KC_I,        KC_O,        KC_P,             KC_BSLASH,
     KC_GRAVE,       KC_A,         KC_S,    KC_D,    KC_F,         KC_G,                                                                                                      KC_H, KC_J,     KC_K,        KC_L,        KC_SCOLON,        KC_QUOTE,
-    KC_LSFT,        LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,         KC_B, MT(MOD_HYPR, KC_ENTER),                                                      MT(MOD_HYPR, KC_ENTER), KC_N, KC_M,     KC_COMMA,    KC_DOT,      RCTL_T(KC_SLASH), KC_RSHIFT,
+    KC_LSFT,        LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,         KC_B, MT(MOD_G_HYPR, KC_ENTER),                                                      MT(MOD_G_HYPR, KC_ENTER), KC_N, KC_M,     KC_COMMA,    KC_DOT,      RCTL_T(KC_SLASH), KC_RSHIFT,
     LT(1,KC_GRAVE), KC_SPC,       KC_LEAD, KC_LALT, LT(3,KC_END),                                                                                                                  KC_EQUAL, KC_LALT,     KC_TRNS,     KC_RCTL,          MO(1),
                                                                                         LT(2, KC_ESC), TG(1),             KC_LOCK,   LCTL_T(KC_ESC),
-                                                                                                       KC_HOME,           KC_PGUP,
+                                                                                        KC_HOME,           KC_PGUP,
                                                                         LGUI_T(KC_SPC), LT(4, KC_ESC), LT(3,KC_END),      KC_PGDOWN, LT(4, KC_ESC),  KC_SPC
   ),
   [1] = LAYOUT_ergodox_pretty(
