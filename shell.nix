34597556281cc28cb5fab87ffade3a84f3fe07e0
Garry Hill <garry@magnetised.net>
2025-06-02 09:09:32 +0100

    fix shell.nix

--- shell.nix
+++ shell.nix
@@ -3,11 +3,14 @@ let
   sources = import ./util/nix/sources.nix { };
 in
 # However, if you want to override Niv's inputs, this will let you do that.
-{ pkgs ? import sources.nixpkgs { }
-, poetry2nix ? pkgs.callPackage (import sources.poetry2nix) { }
-, avr ? true
-, arm ? true
-, teensy ? true }:
+{
+  # pkgs ? import sources.nixpkgs { },
+  pkgs ? import <nixpkgs> { },
+  poetry2nix ? pkgs.callPackage (import sources.poetry2nix) { },
+  avr ? true,
+  arm ? true,
+  teensy ? true,
+}:
 with pkgs;
 let
   avrlibc = pkgsCross.avr.libcCross;
@@ -26,41 +29,46 @@ let
   # nix/poetry.lock Use the "poetry update --lock", "poetry add
   # --lock" etc. in the nix folder to adjust the contents of those
   # files if the requirements*.txt files change
-  pythonEnv = poetry2nix.mkPoetryEnv {
-    projectDir = ./util/nix;
-    overrides = poetry2nix.overrides.withDefaults (self: super: {
-      qmk = super.qmk.overridePythonAttrs(old: {
-        # Allow QMK CLI to run "qmk" as a subprocess (the wrapper changes
-        # $PATH and breaks these invocations).
-        dontWrapPythonPrograms = true;
-
-        # Fix "qmk setup" to use the Python interpreter from the environment
-        # when invoking "qmk doctor" (sys.executable gets its value from
-        # $NIX_PYTHONEXECUTABLE, which is set by the "qmk" wrapper from the
-        # Python environment, so "qmk doctor" then runs with the proper
-        # $NIX_PYTHONPATH too, because sys.executable actually points to
-        # another wrapper from the same Python environment).
-        postPatch = ''
-          substituteInPlace qmk_cli/subcommands/setup.py \
-            --replace "[Path(sys.argv[0]).as_posix()" \
-              "[Path(sys.executable).as_posix(), Path(sys.argv[0]).as_posix()"
-        '';
-      });
-    });
-  };
+  # pythonEnv = poetry2nix.mkPoetryEnv {
+  #   projectDir = ./util/nix;
+  #   overrides = poetry2nix.overrides.withDefaults (self: super: {
+  #     qmk = super.qmk.overridePythonAttrs(old: {
+  #       # Allow QMK CLI to run "qmk" as a subprocess (the wrapper changes
+  #       # $PATH and breaks these invocations).
+  #       dontWrapPythonPrograms = true;
+  #     });
+  #   });
+  # };
 in
 mkShell {
   name = "qmk-firmware";
 
-  buildInputs = [ clang-tools_11 dfu-programmer dfu-util diffutils git pythonEnv niv ]
+  # buildInputs = [ clang-tools dfu-programmer dfu-util diffutils git pythonEnv poetry niv ]
+  buildInputs =
+    [
+      clang-tools_11
+      dfu-programmer
+      dfu-util
+      diffutils
+      git
+      poetry
+      niv
+    ]
     ++ lib.optional avr [
       pkgsCross.avr.buildPackages.binutils
-      pkgsCross.avr.buildPackages.gcc8
+      # pkgsCross.avr.buildPackages.gcc8
+      pkgsCross.avr.buildPackages.gcc
       avrlibc
       avrdude
     ]
     ++ lib.optional arm [ gcc-arm-embedded ]
     ++ lib.optional teensy [ teensy-loader-cli ];
+
+  nativeBuildInputs = [
+    qmk
+    python312Packages.pip
+    python312Packages.appdirs
+  ];
 
   AVR_CFLAGS = lib.optional avr avr_incflags;
   AVR_ASFLAGS = lib.optional avr avr_incflags;
