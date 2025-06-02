1920c77de46a21ea6ae57a7a22c57534c63c8b35
Garry Hill <garry@magnetised.net>
2025-06-02 13:31:49 +0100

    rebase on main zsa code

--- keyboards/zsa/ergodox_ez/keymaps/garry/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/garry/keymap.c
@@ -30,7 +30,7 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM underscore_combo[] = {KC_F, KC_J, COMBO_END};
-const uint16_t PROGMEM ctrl_a_combo[] = {KC_A, KC_SCOLON, COMBO_END};
+const uint16_t PROGMEM ctrl_a_combo[] = {KC_A, KC_SEMICOLON, COMBO_END};
 const uint16_t PROGMEM sq_brackets_combo[] = {KC_G, KC_H, COMBO_END};
 const uint16_t PROGMEM percent_combo[] = {KC_D, KC_K, COMBO_END};
 const uint16_t PROGMEM paren_combo[] = {KC_S, KC_L, COMBO_END};
@@ -48,19 +48,19 @@ combo_t key_combos[COMBO_COUNT] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_ESC,         KC_1,         KC_2,    KC_3,    KC_4,         KC_5, KC_DELETE,                                                                   KC_DELETE,              KC_6, KC_7,     KC_8,        KC_9,        KC_0,             KC_MINUS,
-    KC_TAB,         KC_Q,         KC_W,    KC_E,    KC_R,         KC_T, KC_BSPACE,                                                                   KC_BSPACE,              KC_Y, KC_U,     KC_I,        KC_O,        KC_P,             KC_BSLASH,
-    KC_GRAVE,       KC_A,         KC_S,    KC_D,    KC_F,         KC_G,                                                                                                      KC_H, KC_J,     KC_K,        KC_L,        KC_SCOLON,        KC_QUOTE,
-    KC_LSFT,        LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,         KC_B, MT(MOD_G_HYPR, KC_ENTER),                                                      MT(MOD_G_HYPR, KC_ENTER), KC_N, KC_M,     KC_COMMA,    KC_DOT,      RCTL_T(KC_SLASH), KC_RSHIFT,
-    LT(1,KC_GRAVE), KC_SPC,       KC_LEAD, KC_LALT, LT(3,KC_END),                                                                                                                  KC_EQUAL, KC_LALT,     KC_TRNS,     KC_RCTL,          MO(1),
-                                                                                        LT(2, KC_ESC), TG(1),             KC_LOCK,   LCTL_T(KC_ESC),
+    KC_TAB,         KC_Q,         KC_W,    KC_E,    KC_R,         KC_T, KC_BACKSPACE,                                                                   KC_BACKSPACE,              KC_Y, KC_U,     KC_I,        KC_O,        KC_P,             KC_BACKSLASH,
+    KC_GRAVE,       KC_A,         KC_S,    KC_D,    KC_F,         KC_G,                                                                                                      KC_H, KC_J,     KC_K,        KC_L,        KC_SEMICOLON,        KC_QUOTE,
+    KC_LSFT,        LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,         KC_B, MT(MOD_G_HYPR, KC_ENTER),                                                      MT(MOD_G_HYPR, KC_ENTER), KC_N, KC_M,     KC_COMMA,    KC_DOT,      RCTL_T(KC_SLASH), KC_RIGHT_SHIFT,
+    LT(1,KC_GRAVE), KC_SPC,       QK_LEAD, KC_LALT, LT(3,KC_END),                                                                                                                  KC_EQUAL, KC_LALT,     KC_TRNS,     KC_RCTL,          MO(1),
+                                                                                        LT(2, KC_ESC), TG(1),             KC_CAPS_LOCK,   LCTL_T(KC_ESC),
                                                                                         KC_HOME,           KC_PGUP,
-                                                                        LGUI_T(KC_SPC), LT(4, KC_ESC), LT(3,KC_END),      KC_PGDOWN, LT(4, KC_ESC),  KC_SPC
+                                                                        LGUI_T(KC_SPC), LT(4, KC_ESC), LT(3,KC_END),      KC_PAGE_DOWN, LT(4, KC_ESC),  KC_SPC
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_ESC,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRNS,                                 KC_TRNS, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
     KC_TRNS,   KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_TRNS,                                 KC_TRNS, KC_UP,          KC_7,           KC_8,           KC_9,           KC_ASTR,        KC_F12,
     KC_TRNS,   KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRAVE,                                                                       KC_DOWN,        KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_TRNS,
-    KC_TRNS,   KC_PERC,        KC_CIRC,        KC_LBRACKET,    KC_RBRACKET,    KC_TILD,        KC_TRNS,                                 KC_TRNS, KC_AMPR,        KC_1,           KC_2,           KC_3,           KC_BSLASH,      KC_TRNS,
+    KC_TRNS,   KC_PERC,        KC_CIRC,        KC_LEFT_BRACKET,    KC_RIGHT_BRACKET,    KC_TILD,        KC_TRNS,                                 KC_TRNS, KC_AMPR,        KC_1,           KC_2,           KC_3,           KC_BACKSLASH,      KC_TRNS,
     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                                                                                 KC_TRNS, KC_DOT,         KC_0,           KC_EQUAL,       KC_TRNS,
                                                                                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                                                                                     KC_TRNS, KC_TRNS,
@@ -84,18 +84,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                                                                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                                                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                                                                                     KC_TRNS, KC_PGUP,
-                                                                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDOWN,      KC_TRNS, KC_TRNS
+                                                                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_PAGE_DOWN,      KC_TRNS, KC_TRNS
 
   ),
   [4] = LAYOUT_ergodox_pretty(
     KC_TRNS,  LSFT(KC_1), LSFT(KC_2),    LSFT(KC_3),     LSFT(KC_4),     LSFT(KC_5), KC_TRNS,                                            KC_TRNS, LSFT(KC_6),    LSFT(KC_7),    LSFT(KC_8),    LSFT(KC_9),     LSFT(KC_0),  KC_UNDERSCORE,
-    KC_TRNS,  KC_TRNS,    KC_LBRC,       GH_ELIXIR_PIPE, KC_RBRC,        KC_BSLASH,  KC_TRNS,                                            KC_TRNS, KC_LBRC,       KC_LCBR,       KC_UNDERSCORE, KC_RCBR,        KC_RBRC,     GH_ELIXIR_PIPE,
+    KC_TRNS,  KC_TRNS,    KC_LBRC,       GH_ELIXIR_PIPE, KC_RBRC,        KC_BACKSLASH,  KC_TRNS,                                            KC_TRNS, KC_LBRC,       KC_LCBR,       KC_UNDERSCORE, KC_RCBR,        KC_RBRC,     GH_ELIXIR_PIPE,
     KC_TILDE, KC_COLON,   KC_LCBR,       KC_PIPE,        KC_RCBR,        KC_PERCENT,                                                              KC_UNDERSCORE, KC_LEFT_PAREN, KC_EQUAL,      KC_RIGHT_PAREN, KC_COLON,    KC_DOUBLE_QUOTE,
     KC_TRNS,  KC_LCTL,    KC_LEFT_PAREN, KC_TRNS,        KC_RIGHT_PAREN, KC_TRNS,    KC_TRNS,                                            KC_TRNS, KC_PLUS,       KC_LT,         KC_MINUS,      KC_GT,          KC_QUESTION, KC_TRNS,
     KC_TRNS,  KC_TRNS,    KC_TRNS,       KC_TRNS,        KC_TRNS,                                                                                                KC_LCBR,       KC_PIPE,       KC_RCBR,        KC_TRNS,     KC_TRNS,
                                                                                               KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,
                                                                                                        KC_TRNS,      KC_PGUP,
-                                                                                     KC_TRNS, KC_TRNS, KC_TRNS,      KC_PGDOWN, KC_TRNS, KC_TRNS
+                                                                                     KC_TRNS, KC_TRNS, KC_TRNS,      KC_PAGE_DOWN, KC_TRNS, KC_TRNS
   ),
   /* [4] = LAYOUT_ergodox_pretty( */
   /*   KC_TRNS, LSFT(KC_1),     LSFT(KC_2),     LSFT(KC_3),     LSFT(KC_4),     LSFT(KC_5),     KC_TRNS,                                 KC_TRNS, LSFT(KC_6),      LSFT(KC_7),     LSFT(KC_8),     LSFT(KC_9),     LSFT(KC_0),     KC_UNDERSCORE, */
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                                                                                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, */
   /*                                                                                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, */
   /*                                                                                                                   KC_TRNS, KC_PGUP, */
-  /*                                                                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDOWN,      KC_TRNS, KC_TRNS */
+  /*                                                                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_PAGE_DOWN,      KC_TRNS, KC_TRNS */
   /* ), */
 };
 
@@ -115,12 +115,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         switch (keycode) {
             case GH_CTRL_U:
                 if (record->event.pressed) {
-                    SEND_STRING(SS_LCTRL("u"));
+                    SEND_STRING(SS_LCTL("u"));
                 }
                 break;
             case GH_CTRL_D:
                 if (record->event.pressed) {
-                    SEND_STRING(SS_LCTRL("d"));
+                    SEND_STRING(SS_LCTL("d"));
                 }
                 break;
             case GH_ELIXIR_PIPE:
@@ -215,11 +215,11 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 /*         leading = false; */
 /*         leader_end(); */
 /*         SEQ_ONE_KEY(KC_A) { */
-/*             SEND_STRING(SS_LCTRL("a") SS_LCTRL("a")); */
+/*             SEND_STRING(SS_LCTL("a") SS_LCTL("a")); */
 /*         } */
 
 /*         SEQ_ONE_KEY(KC_U) { */
-/*             SEND_STRING(SS_LCTRL("a") "["); */
+/*             SEND_STRING(SS_LCTL("a") "["); */
 /*         } */
 /*     } */
 /* } */
